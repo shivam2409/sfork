76af738c74f90e90ebdfc60bbcd04d547e238e59
Shivam Patel <51165698+shivam2409@users.noreply.github.com>
2020-07-28 12:51:03 -0400

    created server

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var path = require('path');
+var bodyparser = require('body-parser');
+var cons = require('consolidate');
+var dust = require('dustjs-helpers');
+var pg = require('pg');
+const bodyParser = require('body-parser');
+var app = express();
+
+//database string
+var connect = 'postgresql://learn:demo@localhost/sforkDB';
+
+//Assign dust engine to .dust files
+app.engine('dust', cons.dust);
+
+//Set Dust as default Ext
+app.set('view engine', 'dust');
+app.set('views', __dirname + '/views');
+
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//route
+app.get('/', function (req, res) {
+  res.render('index');
+});
+
+//Server
+app.listen(3000, function () {
+  console.log('Server started on Port 3000');
+});
